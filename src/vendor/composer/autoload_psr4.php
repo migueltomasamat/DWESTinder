a675a190eb7171b5d505756166670caec125a06d
migueltomasamat <migueltomas.informatica@iespacomolla.es>
2023-11-09 15:47:30 +0100

    Añadido config de virtuaHost

--- src/vendor/composer/autoload_psr4.php
+++ src/vendor/composer/autoload_psr4.php
@@ -6,6 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    '\\App' => array($baseDir . '/app'),
     'Symfony\\Polyfill\\Php80\\' => array($vendorDir . '/symfony/polyfill-php80'),
     'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
     'Symfony\\Contracts\\Translation\\' => array($vendorDir . '/symfony/translation-contracts'),
